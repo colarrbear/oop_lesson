a36f094fa1d479a28dbdd330295744cc191a7be6
Chayakarn Hengsuwan <chayakarn.h@ku.th>
2023-11-02 10:04:10 +0700

    OO bank account simulation fix

--- bank_account.py
+++ bank_account.py
@@ -7,19 +7,27 @@ class AccountDB:
         if index == -1:
             self.account_database.append(account)
         else:
-            print(account, "Duplicated account; nothing to be insert")
+            print(account.account_number, "Duplicated account; nothing to be inserted")
 
     def __search_private(self, account_num):
-        for i in range(len(self.account_database)):
-            if self.account_database[i].account_number == account_num:
+        for i, acc in enumerate(self.account_database):
+            if acc.account_number == account_num:
                 return i
         return -1
 
     def search_public(self, account_num):
-        for account in self.account_database:
-            if account.account_number == account_num:
-                return account
+        for acc in self.account_database:
+            if acc.account_number == account_num:
+                return acc
         return None
+
+    def delete_account(self, account_num):
+        account = self.search_public(account_num)
+        if account:
+            print("Deleting account:", account_num)
+            self.account_database.remove(account)
+        else:
+            print(account_num, "invalid account number; nothing to be removed.")
 
     def __str__(self):
         s = ''
@@ -36,33 +44,104 @@ class Account:
         self.balance = balance
 
     def deposit(self, amount):
-        self.balance += amount
+        if self:
+            print("Depositing", amount, "to", self.account_number)
+            self.balance += amount
+        else:
+            print("Invalid account; no deposit action performed.")
 
     def withdraw(self, amount):
-        if self.balance >= amount:
-            self.balance -= amount
+        if self:
+            if self.balance >= amount:
+                print("Withdrawing", amount, "from", self.account_number)
+                self.balance -= amount
+            else:
+                print("Withdrawal amount", amount, "exceeds the balance of", self.balance, "for", self.account_number, "account.")
+        else:
+            print("Invalid account; no withdrawal action performed.")
 
     def __str__(self):
-        return '{' + str(self.account_number) + ',' + str(
-            self.type) + ',' + str(self.account_name) + ',' + str(
-            self.balance) + '}'
+        return '{' + str(self.account_number) + ',' + str(self.type) + ',' + str(self.account_name) + ',' + str(self.balance) + '}'
 
 
-account1 = Account("0000", "saving", "David Patterson", 1000)
-account2 = Account("0001", "checking", "John Hennessy", 2000)
-account3 = Account("0003", "saving", "Mark Hill", 3000)
-account4 = Account("0004", "saving", "David Wood", 4000)
-account5 = Account("0004", "saving", "David Wood", 4000)
-my_account_DB = AccountDB()
-my_account_DB.insert(account1)
-my_account_DB.insert(account2)
-my_account_DB.insert(account3)
-my_account_DB.insert(account4)
-my_account_DB.insert(account5)
-print(my_account_DB)
-my_account_DB.search_public("0003").deposit(50)
-print(my_account_DB)
-my_account_DB.search_public("0003").withdraw(100)
-print(my_account_DB)
-my_account_DB.search_public("0010").deposit(50)
-print(my_account_DB)
+if __name__ == "__main__":
+    my_account_DB = AccountDB()
+
+    account1 = Account("0000", "saving", "David Patterson", 1000)
+    account2 = Account("0001", "checking", "John Hennessy", 2000)
+    account3 = Account("0003", "saving", "Mark Hill", 3000)
+    account4 = Account("0004", "saving", "David Wood", 4000)
+    account5 = Account("0004", "saving", "David Wood", 4000)
+
+    my_account_DB.insert(account1)
+    my_account_DB.insert(account2)
+    my_account_DB.insert(account3)
+    my_account_DB.insert(account4)
+    my_account_DB.insert(account5)
+
+    print(my_account_DB)
+
+    account = my_account_DB.search_public("0003")
+    if account:
+        account.deposit(50)
+    else:
+        print("Account not found.")
+
+    print(my_account_DB)
+
+    account = my_account_DB.search_public("0003")
+    if account:
+        account.withdraw(25)
+    else:
+        print("Account not found.")
+
+    print(my_account_DB)
+
+    my_account_DB.delete_account("0003")
+
+    account = my_account_DB.search_public("0003")
+    if account:
+        account.deposit(50)
+    else:
+        print("Account not found.")
+
+    account = my_account_DB.search_public("0001")
+    if account:
+        account.withdraw(6000)
+    else:
+        print("Account not found.")
+    # account1 = Account("0000", "saving", "David Patterson", 1000)
+    # account2 = Account("0001", "checking", "John Hennessy", 2000)
+    # account3 = Account("0003", "saving", "Mark Hill", 3000)
+    # account4 = Account("0004", "saving", "David Wood", 4000)
+    # account5 = Account("0004", "saving", "David Wood", 4000)
+    #
+    # my_account_DB = AccountDB()
+    # my_account_DB.insert(account1)
+    # my_account_DB.insert(account2)
+    # my_account_DB.insert(account3)
+    # my_account_DB.insert(account4)
+    # my_account_DB.insert(account5)
+    # print(my_account_DB)
+    #
+    # account = my_account_DB.search_public("0003")
+    # if account:
+    #     account.deposit(50)
+    # else:
+    #     print("Account not found.")
+    #
+    # print(my_account_DB)
+    #
+    # account = my_account_DB.search_public("0003")
+    # if account:
+    #     account.withdraw(100)
+    # else:
+    #     print("Account not found.")
+    #
+    # print(my_account_DB)
+    #
+    # account = my_account_DB.search_public("0010")
+    # if account:
+    #     account.deposit(50)
+    # else:
+    #     print("Account not found.")
